34cac3898039083bc6abe05e3123b61d4b11ba39
Magnus Engdahl <kaffebreak@gmail.com>
2022-11-29 09:19:49 +0100

    Created Car

---
+++ BranchesGroup/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchesGroup
+{
+    internal class Car
+    {
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public Car(string model, string make)
+        {
+            Model = model;
+            Make = make;
+        }
+
+    }
+}
